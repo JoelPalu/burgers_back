8853c64fb48d4b44142d39c734fc793f07a1462d
JoelPalu <kirill.saveliev@metropolia.fi>
2024-04-19 10:17:25 +0300

    add router to uploads to fetch images

--- src/app.js
+++ src/app.js
@@ -7,9 +7,16 @@ import {
 } from './middlewares/middlewares.js';
 import cors from 'cors';
 
-const hostname = '127.0.0.1';
+//Server
+const hostname ='10.120.32.57'
+const port = 80;
+
+//Localhost
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
 const app = express();
-const port = 3000;
+
 app.use(cors());
 
 app.use('/public', express.static('public'));

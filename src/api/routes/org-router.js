846258d38411c85677ca73786c1ebc2ce953375e
kiril <kirillsa@metropolia.fi>
2025-12-15 13:13:54 +0200

    Add documentation

--- src/api/routes/org-router.js
+++ src/api/routes/org-router.js
@@ -3,6 +3,13 @@ import {getOrgs} from '../controllers/org-controller.js';
 
 const orgRouter = express.Router();
 
+/*
+  This whole section is for future expansion.
+  Currently, we only have a GET endpoint to retrieve organizations.
+  We add organizations through database seeding.
+  Organisation used to what device is assigned to which org. As well could be used to check what users are part of which org.
+*/
+
 orgRouter.route('/')
   .get(getOrgs);
 

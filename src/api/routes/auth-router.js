0efe52901876406aef85146271f3f17fb8feff6e
kiril <kirillsa@metropolia.fi>
2025-12-15 14:29:35 +0200

    Revert "Add documentation for auth router"
    This reverts commit c9ee4264dd5b07dfafa9a11f5b28eb7e216e4db4.

--- src/api/routes/auth-router.js
+++ src/api/routes/auth-router.js
@@ -7,14 +7,8 @@ import {
 
 
 const authRouter = express.Router();
-/*
-  This section handles authentication-related routes.
-  We have endpoints for logging in, logging out, retrieving the current user's information,
- */
-// This one will receive username and password, validate them, and return a JWT token if valid.
-authRouter.route('/login').post(postLogin);
 
-// This one is just a placeholder. Actual logout logic (like token invalidation) would be handled on the client side or with a token blacklist.
+authRouter.route('/login').post(postLogin);
 authRouter.route('/logout').get(getLogOut);
 
 authRouter.route('/me').get(authenticateToken, validationErrors, getMe);
